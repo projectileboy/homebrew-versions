8ca95758b3cba320f2bd4a323892d5919dec6ed0
Kurt Christensen <kurt.j.christensen@gmail.com>
2015-04-21 16:22:09 -0500

    Fixed issues raised by brwe audit --strict
    I changed the sha1 check to a sha256 check, and added a very simple test do block. I also made some trivial style changes suggested by brew audit (removed whitespace, and changed single quotes to double quotes where appropriate).

--- gecode373.rb
+++ gecode373.rb
@@ -1,12 +1,14 @@
-require 'formula'
-
-class Gecode < Formula
-  homepage 'http://www.gecode.org/'
-  url 'http://www.gecode.org/download/gecode-3.7.3.tar.gz'
-  sha1 'f9281a80788cd0c0f0495cd6145fe4bf9ee74117'
+class Gecode373 < Formula
+  homepage "http://www.gecode.org/"
+  url "http://www.gecode.org/download/gecode-3.7.3.tar.gz"
+  sha256 "e7cc8bcc18b49195fef0544061bdd2e484a1240923e4e85fa39e8d6bb492854c"
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-examples"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/fz", "-help"
   end
 end
